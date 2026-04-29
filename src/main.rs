6ce482adc902b5956eb1cc678aae4f04f4ffa631
nukr <me@n-u.kr>
2026-04-29 21:55:49 +0800

    add https support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::{net::SocketAddr, path::PathBuf};
+
 use axum::{Router, routing::get};
+use axum_server::tls_rustls::RustlsConfig;
 use clap::Parser;
 use tower::ServiceExt;
 use tower_http::services::ServeDir;
@@ -15,6 +18,17 @@ async fn main() {
     tracing_subscriber::fmt::init();
     let args = Args::parse();
     let assets = ServeDir::new("loan-html/assets");
+    // configure certificate and private key used by https
+    let config = RustlsConfig::from_pem_file(
+        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("self_signed_certs")
+            .join("cert.pem"),
+        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("self_signed_certs")
+            .join("key.pem"),
+    )
+    .await
+    .unwrap();
     let app = Router::new()
         // `GET /` goes to `root`
         .route(
@@ -27,10 +41,14 @@ async fn main() {
         )
         .nest_service("/assets", assets.clone());
 
-    let addr = format!("0.0.0.0:{}", args.port);
+    let addr = SocketAddr::from(([127, 0, 0, 1], args.port));
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    let _ = axum::serve(listener, app).await;
+    // let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    // let _ = axum::serve(listener, app).await;
 }
 
 async fn statistics() {}
